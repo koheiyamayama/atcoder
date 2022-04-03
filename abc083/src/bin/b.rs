3b6c6c6e22e8e29287154e7fdf52fd39f7a465e2
koheiyamayama <kohei.y.23@gmail.com>
2022-04-04 00:49:20 +0900

    abc083b解いた

--- abc083/src/bin/b.rs
+++ abc083/src/bin/b.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
+fn digit_sum(num: i32) -> i32 {
+    num
+        .to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap() as i32)
+        .collect::<Vec<_>>()
+        .iter()
+        .sum()
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        a: i32,
+        b: i32
+    }
+
+    let mut nums: Vec<i32> = vec!();
+    for num in 0..=n {
+        let digit_sum = digit_sum(num);
+        if digit_sum >= a && digit_sum <= b {
+            nums.push(num)
+        };
+    }
+
+    println!("{}", nums.iter().sum::<i32>())
 }
