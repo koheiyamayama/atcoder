ae27e65b00d145cc3b19374aea217e2543a46fcf
koheiyamayama <kohei.y.23@gmail.com>
2022-07-04 22:11:36 +0900

    abc049b解いた

--- abc049/src/bin/b.rs
+++ abc049/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: u8,
+        w: u8,
+        strings: [String; h]
+    }
+
+    for s in strings.iter() {
+        println!("{}", s);
+        println!("{}", s);
+    }
 }
