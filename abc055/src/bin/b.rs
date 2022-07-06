a25dad2e5105ee8f29da4f7b1a9ce0a88dbee747
koheiyamayama <kohei.y.23@gmail.com>
2022-07-06 23:03:28 +0900

    abc055bとcを解いた

--- abc055/src/bin/b.rs
+++ abc055/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u128
+    }
+    
+    let m: u128 = 10u128.pow(9u32) + 7;
+    let mut result: u128 = 1;
+    for i in 1..=n {
+        result *= i;
+        result %= m;
+    }
+
+    
+    println!("{}", result);
 }
--- abc055/src/bin/c.rs
+++ abc055/src/bin/c.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        m: i64
+    }
+
+    let r: i64;
+    if n * 2 <= m {
+        r = n + (m - (n * 2)) / 4;
+    } else {
+        r = m / 2
+    }
+
+    println!("{}", r)
 }
