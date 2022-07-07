98189e967c0e9de459618de3514f1571ca35150b
koheiyamayama <kohei.y.23@gmail.com>
2022-07-08 02:08:13 +0900

    math-and-algorith014と015を解いた

--- math-and-algorithm/src/bin/014.rs
+++ math-and-algorithm/src/bin/014.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: f64
+    }
+
+    
+    let s = n.sqrt().floor() as i64;
+    let mut n = n as i64;
+    let mut vec = vec!();
+
+    for i in 2..=s {
+        while n % i == 0 {
+            vec.push(i.to_string());
+            n /= i;
+        }
+    }
+
+    if s <= n {
+        vec.push(n.to_string());
+    }
+
+    println!("{}", vec.join(" "));
 }
--- math-and-algorithm/src/bin/015.rs
+++ math-and-algorithm/src/bin/015.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut a: isize,
+        mut b: isize
+    }
+
+    let mut is_zero = false;
+    while !is_zero {
+        if a <= b {
+            b = b % a;
+        } else {
+            a = a % b;
+        }
+
+        if a == 0 || b == 0 {
+            is_zero = true
+        }
+    }
+
+    if a <= b {
+        print!("{}", b)
+    } else {
+        print!("{}", a)
+    }
 }
