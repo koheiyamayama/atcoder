fc65c5e9f42bf6ce2c0e1a78b26ebc07effa7f39
koheiyamayama <kohei.y.23@gmail.com>
2022-07-21 00:16:16 +0900

    動的計画法難しい、、、まじで、、、

--- math-and-algorithm/src/bin/009.rs
+++ math-and-algorithm/src/bin/009.rs
@@ -1,3 +1,42 @@
+use std::vec;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: usize,
+        c: [usize; n]
+    }
+
+    let mut dp = vec![vec![false; s+1]; n+1];
+
+    for i in 0..=n {
+        for m in 0..=s {
+            if i == 0 {
+                if m == 0 {
+                    dp[i][m] = true;
+                    break;
+                }
+            }
+
+            if m < c[i-1] {
+                dp[i][m] = dp[i-1][m];
+            }
+
+            if m >= c[i-1] {
+                if dp[i-1][m] == true || dp[i-1][m-c[i-1]] {
+                    dp[i][m] = true
+                } else {
+                    dp[i][m] = false
+                }
+            }
+        }
+    }
+
+    if dp[n][s] {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
--- math-and-algorithm/src/bin/031.rs
+++ math-and-algorithm/src/bin/031.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut dp1: Vec<usize> = vec![0; n+1];
+    let mut dp2: Vec<usize> = vec![0; n+1];
+
+    for i in 1..=n {
+        dp1[i] = a[i-1] + dp2[i-1];
+        dp2[i] = max(dp2[i-1], dp1[i-1]);
+    }
+
+    if dp1[n] <= dp2[n] {
+        println!("{}", dp2[n])
+    } else {
+        println!("{}", dp1[n])
+    }
+}
+
+fn max(a: usize, b: usize) -> usize {
+    if a <= b {
+        return b
+    } else {
+        return a
+    }
 }
