c5713077d3ff0551fcca57b251ead48479e3ee66
koheiyamayama <kohei.y.23@gmail.com>
2022-07-28 10:43:41 +0900

    043Cと047C解いた、とちらも解説AC、、、

--- abc043/src/bin/c.rs
+++ abc043/src/bin/c.rs
@@ -1,3 +1,35 @@
+// こちら参考にした
+// https://drken1215.hatenablog.com/entry/2020/10/29/024800
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [isize; n]
+    }
+
+
+    let mut result = std::isize::MAX;
+    for x in -100..=100 {
+        let mut tmp = 0;
+        for i in 0..n {
+            // a[i]の値を-100~100に書き換えるときのコストの総和を全探索する。
+            // つまり、a[i]の値を-100に書き換えるときのコストの総和を計算する。
+            tmp += (x - a[i]) * (x - a[i]);
+        }
+        // tmpには-100~100までのコストの総和があるので、-100のときと-99のときでどちらの総和が小さいのか比較する。
+        // これを-100~100まで繰り返すことで最小のコストの総和が求められる。
+        result = min(result, tmp);
+    }
+
+    println!("{}" , result)
+}
+
+fn min(n: isize, m: isize) -> isize {
+    if n <= m {
+        return n
+    } else {
+        return m
+    }
 }
--- abc047/src/bin/c.rs
+++ abc047/src/bin/c.rs
@@ -1,3 +1,50 @@
+use std::collections::HashSet;
+
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    // input! {
+    //     mut s: Chars
+    // }
+
+    // let mut count = 0;
+    // let mut tmp: Vec<char> = Vec::new();
+    // for c in s {
+    //     if tmp.last() == None {
+    //         tmp.push(c);
+    //     } else if *tmp.last().unwrap() == c {
+    //         tmp.push(c);
+    //     } else {
+    //         count += 1;
+    //         tmp.push(c);
+    //     }
+    // }
+
+    // println!("{}", count)
+
+    // こちらの回答がわかりやすかったし、スマートだった
+    // https://atcoder.jp/contests/abc047/submissions/32147630
+
+    input! {
+        mut s: Chars,
+    }
+
+    let mut result = 0;
+
+    // 先頭の文字を取得
+    let mut prev = s[0];
+
+    for &c in &s {
+        // 一つ前の文字と先頭から取り出した文字が異なる場合は
+        // prevを更新し、resultをインクリメントする
+        if prev == c {
+            // do noting.
+        } else {
+            prev = c;
+            result += 1;
+        }
+    }
+
+    println!("{}", result);
 }
