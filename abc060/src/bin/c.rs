d43681174ee91f79b0f2d64ae56c167c2592ae75
koheiyamayama <kohei.y.23@gmail.com>
2022-08-03 03:24:51 +0900

    abc60

--- abc060/src/bin/c.rs
+++ abc060/src/bin/c.rs
@@ -9,17 +9,31 @@ fn main() {
 
     let mut r = 0;
 
+    // for i in 1..n {
+    //     if tt[i] - tt[i - 1] >= t {
+    //         r = r + t;
+    //     } else {
+    //         r = tt[i] - tt[i - 1] + r;
+    //     }
+    // }
+
+    // if tt.len() == 1 {
+    //     println!("{}", t)
+    // } else {
+    //     println!("{}", r + t)
+    // }
+
     for i in 1..n {
-        if tt[i] - tt[i - 1] >= t {
-            r = r + t;
-        } else {
-            r = tt[i] - tt[i - 1] + r;
-        }
+        r += min(tt[i] - tt[i - 1], t);
     }
 
-    if tt.len() == 1 {
-        println!("{}", t)
+    println!("{}", r + t)
+}
+
+fn min(n: usize, m: usize) -> usize {
+    if n <= m {
+        return n;
     } else {
-        println!("{}", r + t)
+        return m;
     }
 }
